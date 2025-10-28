97ea0d10e96cb46b155f46bbe8ea5f3ee2fcc0bc
pervez107 <pervezahmmed107@gmail.com>
2025-10-28 11:57:49 +0530

    creating home and other pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import "./App.css";
 import Home from "./pages/Home";
+import Grocery from "./pages/Grocery/GroceryDashboard";
+import Alcohol from "./pages/Alcohol/AlcoholDashboard";
 
 function App() {
 
@@ -12,7 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/grocery-dashboard" element={<Grocery />} />
+        <Route path="/alcohol-dashboard" element={<Alcohol />} />
       </Routes>
     </div>
   );
